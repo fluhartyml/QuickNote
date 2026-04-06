6f45a5c8d0bf6f14ef03ebbdef93bfd662849002
Michael Fluharty <michael@fluharty.com>
2026-04-06 10:25:42 -0500

    Initial Commit

---
+++ QuickNote/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  QuickNote
+//
+//  Created by Michael Fluharty on 4/6/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ QuickNote/QuickNoteApp.swift
@@ -0,0 +1,32 @@
+//
+//  QuickNoteApp.swift
+//  QuickNote
+//
+//  Created by Michael Fluharty on 4/6/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct QuickNoteApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
